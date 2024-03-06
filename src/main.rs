fdee55a4f332f12342aee88cb9ec1dc805dc2f08
Skylar Cook <skylar.cook@noom.com>
2024-03-05 23:24:03 -0800

    write hello to VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,5 +12,16 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // Cast buffer location to raw pointer.
+    let vga_buffer = 0xb800 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // Write byte and color to buffer.
+        // Unsafe because we're dealing with raw pointers.
+        // Ideally we'd encapsulate in some sort of VGA type!
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
