5a927615588034971a97362923c7431e4963fba4
Skylar Cook <skylar.cook@noom.com>
2024-04-02 11:29:43 -0700

    encapsulate VGA into module

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,16 +12,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Cast buffer location to raw pointer.
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // Write byte and color to buffer.
-        // Unsafe because we're dealing with raw pointers.
-        // Ideally we'd encapsulate in some sort of VGA type!
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
