02cfb537a247eb9898e8dfd57c86f51088d4a15b
Skylar Cook <skylar.cook@noom.com>
2024-04-03 10:50:34 -0700

    add test runner and export entry as test_main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,6 +24,17 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hullo, Wörld!");
-    println!("Numbers! {} and {}\n ", 42, 1.0 / 3.0);
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[test_case]
+fn it_works() {
+    print!("it works... ");
+    assert_eq!(1,1);
+    println!("[ok]")
 }
