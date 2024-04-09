6a4369d46591f862f883770a4d51925cc775fcf1
Skylar Cook <scook90@gmail.com>
2024-04-09 11:16:25 -0700

    add timer handler and End-Of-Interrupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
 // TODO read blog series on naked functions in Rust!
 // https://os.phil-opp.com/edition-1/extra/naked-exceptions/
 use crate::gdt;
+use crate::print;
 use crate::println;
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -8,6 +9,23 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+// TODO can we use `impl From<InterruptIndex> for u8`?
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
 
 // Remap Programmable Interrupt Controller vector numbers to 32-47 because
 // we already use 0-15 for CPU exceptions.
@@ -25,6 +43,7 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -43,6 +62,15 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    // TODO this can deadlock on global WRITER with non-interrupt writes.
+    print!(".");
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 
 #[test_case]
