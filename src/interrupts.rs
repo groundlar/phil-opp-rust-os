d1f84282df928dbd6f0e3fbe243aae93cd44fd4c
Skylar Cook <scook90@gmail.com>
2024-04-10 11:54:40 -0700

    display numeric key presses

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -93,7 +93,24 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     let mut port = Port::new(PortValues::PS2.into());
     let scancode: u8 = unsafe { port.read() };
     // NOTE: this can deadlock on global WRITER with non-interrupt prints.
-    print!("{}", scancode);
+
+    if let Some(key) = match scancode {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    } {
+        print!("{}", key);
+    } else {
+        print!("UNKK");
+    }
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.into());
