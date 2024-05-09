54e47d478bc47b35219e5b0fda1b8f7ffbc21354
Skylar Cook <scook90@gmail.com>
2024-05-09 15:38:42 -0700

    print l4 table addr

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use core::panic::PanicInfo;
 use phil_opp_rust_os::{print, println};
+use x86_64::registers::control::Cr3;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -26,11 +27,13 @@ pub extern "C" fn _start() -> ! {
 
     phil_opp_rust_os::init();
 
-    // kablooie
-    let ptr = 0xDEADBEEF as *mut u8;
-    unsafe {
-        *ptr = 42;
-    }
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
+    // PhysAddr(0x1000)
+    // We can't access this directly for obvious reasons.
 
     #[cfg(test)]
     test_main();
