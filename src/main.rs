5f40d18f749689e3bb1010d6e07b6968602e094e
Skylar Cook <scook90@gmail.com>
2024-05-14 14:51:58 -0700

    demo next level translation

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use phil_opp_rust_os::{memory::active_level_4_table, print, println};
-use x86_64::{registers::control::Cr3, VirtAddr};
+use x86_64::{registers::control::Cr3, structures::paging::PageTable, VirtAddr};
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -35,6 +35,16 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         // These should map to different L3 entries.
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("    L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
